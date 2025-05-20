9b9b5e95eb62abbe62716a19f18fb0d4b2925a4b
Bildad Ereggae <bildadereggae@gmail.com>
2025-05-20 19:06:33 +0300

    functions lab done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
+
+greetprogrammer = greet_programmer()
+print(greetprogrammer)
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!") 
+
+greet("bildad")
 
 def greet_with_default(name="programmer"):
-    pass
+    print (f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Bildad")
+
+print(greet_with_default)
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
